1db308f61ee0591b753716140b1dcaf59655012e
hps <ananda.radnaev.19@mail.ru>
2024-04-28 20:32:50 +0900

    Merge projects

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import csv
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.utils import get
@@ -7,12 +8,18 @@ from discord import FFmpegPCMAudio
 from discord import TextChannel
 from youtube_dl import YoutubeDL
 from config import TOKEN
-from discord.components import Button, ButtonStyle
 
 load_dotenv()
 # назначение префикса для команд
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 players = {}
+
+
+# with open('musics.csv', mode='w', encoding='utf-8') as m_file:
+#     names = ["название", "ссылка"]
+#     file_writer = csv.DictWriter(m_file, delimiter=",",
+#                                  lineterminator="\r", fieldnames=names)
+#     file_writer.writeheader()
 
 
 # Проверка коммита
@@ -62,24 +69,63 @@ class MyView(discord.ui.View):
 
 
 # команда для воспроизведения звука с URL-адреса youtube
-
-
 @client.command()
-async def play(ctx, url):
+async def play(ctx, url, name_title=None):
     YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
     FFMPEG_OPTIONS = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
     voice = get(client.voice_clients, guild=ctx.guild)
 
+    # вход в голосовой канал
+    channel = ctx.message.author.voice.channel
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
     if not voice.is_playing():
+        count = 0
+        # проверка плейлиста
+        with open('musics.csv', mode='r', encoding='utf-8') as m_file:
+            file_reader = csv.reader(m_file)
+            for row in file_reader:
+                if url == row[-1]:
+                    await ctx.send(f"Этот трек есть в листе, название - {row[0]}")
+                    count += 1
+                if url == row[0]:
+                    url = row[-1]
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
+        name = info['title']
+        name = ''.join(name.split())
+
+        # добавление трека в в плейлист
+        if name_title and count == 0:
+            a = 0
+            with open('musics.csv', mode='r', encoding='utf-8') as m_file:
+                file_reader = csv.reader(m_file)
+                for row in file_reader:
+                    if row == ["название", "ссылка"]:
+                        pass
+                    else:
+                        if name_title in row:
+                            if name_title == row[0]:
+                                await ctx.send("Это название уже используется, придумайте другое")
+                                a += 1
+                        else:
+                            if a == 0:
+                                with open('musics.csv', mode='a', encoding='utf-8') as m_file:
+                                    names = ["название", "ссылка"]
+                                    file_writer = csv.DictWriter(m_file, delimiter=",", lineterminator="\r",
+                                                                 fieldnames=names)
+                                    file_writer.writerow({"название": name_title, "ссылка": url})
+                                    a += 1
+
         URL = info['url']
         voice.play(discord.FFmpegPCMAudio(URL, executable="ffmpeg/ffmpeg.exe", **FFMPEG_OPTIONS))
         voice.is_playing()
-        await ctx.send('Работает', view=MyView(ctx))
-
-        # бот уже играет музыку
+        await ctx.send('ОНО РАБОТАЕТ!!! 0_0', view=MyView(ctx))
+    # бот уже играет музыку
     else:
         await ctx.send("Бот уже играет другую музыку")
         return
@@ -110,14 +156,15 @@ async def stop_from_button(ctx):
     if voice.is_playing():
         voice.stop()
         await ctx.send(f'Музыка OF...')
-# команда для остановки звука
 
+
+# команда для остановки звука
 @client.command()
 async def stop(ctx):
     voice = get(client.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.stop()
-        await ctx.send(f'Музыка OF...')
+        await ctx.send(f'Музыка OF')
 
 
 # команда для очистки сообщений канала
